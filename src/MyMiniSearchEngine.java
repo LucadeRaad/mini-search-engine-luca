848e21467293d15408de8f7f5c45a40944baa1bc
LucadeRaad <59776518+LucadeRaad@users.noreply.github.com>
2020-03-04 22:19:35 -0800

    testing to see if this works

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -18,7 +18,32 @@ public class MyMiniSearchEngine {
     // each item in the List is considered a document.
     // assume documents only contain alphabetical words separated by white spaces.
     private void index(List<String> texts) {
-        //homework
+        indexes = new HashMap<>();
+        List<List<Integer>> document = new ArrayList<>();
+
+        // We will be putting two arraylists, one holds the document, and the other holds every location that the word appears in the document
+        //for(int i = 0; i < texts.size(); i++)
+
+
+        // this splits up and sets up the keys. now need to have the values
+        for(int i = 0; i < texts.size(); i++) {
+            // Adding new arraylists
+            ArrayList thisDocument = new ArrayList<>();
+            thisDocument.add(i);
+            document.add(thisDocument);
+            // splits up the list of strings into an array
+            String sentence = texts.get(i);
+            String[] words = sentence.split("\\s+"); // Took from stackoverflow
+            // Can go through each individual element of the array
+            for (int j = 0; j < words.length; j++) { // Took from class
+                if (indexes.containsKey(words[j])) {
+                    thisDocument.add(j);
+                    indexes.put(words[j], document);
+                }
+                indexes.put(words[j], document);
+            }
+        }
+        System.out.println(indexes.toString());
     }
 
     // search(key) return all the document ids where the given key phrase appears.
