cc2c72f7e893cc86c72dec0ac87eb08198a89e97
LucadeRaad <59776518+LucadeRaad@users.noreply.github.com>
2020-03-05 23:42:49 -0800

    wasn't able to fix search fully. This has been a sad day still happy about how indexing went but it too long to bugfix.

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -1,22 +1,38 @@
 import java.util.ArrayList;
+
+import java.util.HashMap;
 import java.util.List;
+
 import java.util.Map;
 
+
 public class MyMiniSearchEngine {
+
     // default solution. OK to change.
+
     // do not change the signature of index()
+
     private Map<String, List<List<Integer>>> indexes;
 
+
     // disable default constructor
+
     private MyMiniSearchEngine() {
+
     }
+
 
     public MyMiniSearchEngine(List<String> documents) {
+
         index(documents);
+
     }
 
+
     // each item in the List is considered a document.
+
     // assume documents only contain alphabetical words separated by white spaces.
+
     private void index(List<String> texts) {
         // Where all info is stored
         indexes = new HashMap<>();
@@ -36,20 +52,65 @@ public class MyMiniSearchEngine {
                 // This is to stop overwriting, If the word we're checking already exists, we need to make sure document has all the old values
                 if (indexes.containsKey(words[j]))
                     document = indexes.get(words[j]);
-                // .get allows for multiple inputs into one arraylist
+                // .get into .add allows for multiple inputs into one arraylist
                 document.get(i).add(j);
 
                 indexes.put(words[j].toLowerCase(), document);
             }
         }
-        System.out.println(indexes.toString());
     }
 
     // search(key) return all the document ids where the given key phrase appears.
+
     // key phrase can have one or two words in English alphabetic characters.
+
     // return an empty list if search() finds no match in all documents.
+
     public List<Integer> search(String keyPhrase) {
-        // homework
-        return new ArrayList<>(); // place holder
+        List<Integer> output = new ArrayList<>();
+        String[] words = keyPhrase.split("\\s+"); // Took from stackoverflow
+        List<List<List<Integer>>> fullDocument = new ArrayList<>();
+        List<List<Integer>> keyDocument = new ArrayList<>(); // For cases when searching for just one word
+
+        // Adds all the key word indexes to a larger list in order of input
+        for (int i = 0; i < words.length; i++) {
+            keyDocument = indexes.get(words[i].toLowerCase());
+            fullDocument.add(keyDocument);
+        }
+
+        // Checking for words that dont exist
+        if(keyDocument == null) return new ArrayList<>();
+
+        // If only one word is found this if statement is triggered
+        if(fullDocument.size() == 1) {
+            for(int i = 0; i < keyDocument.size(); i++) {
+                if(!keyDocument.get(i).isEmpty()) output.add(i);
+            }
+        }
+
+        // The indexes in full document now if in order are the same number
+        for (int i = 0; i < fullDocument.size(); i++) {
+            for (int j = 0; j < fullDocument.get(i).size(); j++) {
+                for (int a = 0; a < fullDocument.get(i).get(j).size(); a++) {
+                    // Inspired by slide 65, on class 8
+                    int replace = fullDocument.get(i).get(j).get(a);
+                    replace -= i;
+                    fullDocument.get(i).get(j).set(a, replace);
+                }
+            }
+        }
+
+        // Compares the first list of fulldocuments against the rest of the documents
+        List<List<Integer>> firstDocument = fullDocument.get(0);
+        for (int i = 1; i < fullDocument.size(); i++) {
+            for (int j = 0; j < fullDocument.get(i).size(); j++) {
+                List<List<Integer>> compareTo = fullDocument.get(i);
+                if(firstDocument.get(j).equals(compareTo.get(j)) && !firstDocument.get(j).isEmpty() && !compareTo.get(j).isEmpty()) {
+                    output.add(j);
+                }
+            }
+        }
+        return output;
     }
+
 }
